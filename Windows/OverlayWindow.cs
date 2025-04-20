a43011a87534ed2aa07b9a8c18dd6c06ee41f079
ryviance <eion.ling0@gmail.com>
2025-04-20 05:16:36 -0700

    Prototyping some overlay functionality

--- Windows/OverlayWindow.cs
+++ Windows/OverlayWindow.cs
@@ -15,15 +15,54 @@ public class OverlayWindow : Window
                                         ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove)
     {
         this.config = config;
+        IsOpen = true;
+        RespectCloseHotkey = false;
     }
+
+    public void Dispose() { }
 
     public override void Draw()
     {
-        if (!config.ThresholdAlerts.HighlightEnabled) return;
+        if (!config.ThresholdAlerts.HighlightEnabled)
+            return;
 
+        ImGui.SetNextWindowPos(new Vector2(10, 300), ImGuiCond.Always);
+        ImGui.SetNextWindowSize(new Vector2(300, 300), ImGuiCond.Always);
+        ImGui.Begin("###HPOverlayWindow", ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoInputs);
+
+        // Show for yourself
+        var player = Plugin.ClientState.LocalPlayer;
+        if (player != null)
+        {
+            float hpPercent = (float)player.CurrentHp / player.MaxHp;
+            if (hpPercent < config.ThresholdRatio)
+            {
+                var color = config.ThresholdAlerts.HighlightColor.ToLower() == "blue"
+                    ? new Vector4(0f, 0.4f, 1f, 0.4f)
+                    : new Vector4(1f, 0f, 0f, 0.4f);
+
+                ImGui.PushStyleColor(ImGuiCol.Button, color);
+                ImGui.Button($"{player.Name.TextValue} LOW HP!");
+                ImGui.PopStyleColor();
+            }
+        }
+
+        // Show for party members
         foreach (var member in Plugin.PartyList)
         {
-            // same drawing logic here
+            float partyHpPercent = (float)member.CurrentHP / member.MaxHP;
+            if (partyHpPercent < config.ThresholdRatio)
+            {
+                var color = config.ThresholdAlerts.HighlightColor.ToLower() == "blue"
+                    ? new Vector4(0f, 0.4f, 1f, 0.4f)
+                    : new Vector4(1f, 0f, 0f, 0.4f);
+
+                ImGui.PushStyleColor(ImGuiCol.Button, color);
+                ImGui.Button($"{member.Name.TextValue} LOW HP!");
+                ImGui.PopStyleColor();
+            }
         }
+
+        ImGui.End();
     }
 }
